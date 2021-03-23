c38e90bf75be74d8db3aa01096ff1f7395b89bf4
Will <kode15333@gmail.com>
2021-03-24 08:42:09 +0900

    map.ts 분리 Type 오류

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,6 @@
 import * as _ from 'lodash';
 import './scss/index.scss'
-import {Loader, LoaderOptions} from "google-maps";
-const options: LoaderOptions = {};
-const loader = new Loader(`${process.env.GOOGLE_MAP_API}`);
-let myPlaces : Array<object> = []
-let googleMap : any
-
-loader.load().then(function (google) {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8,
-    });
-    map.addListener('click', addPlace)
-});
+import {init} from "./map";
 
 
-function addPlace (event : google.maps.MapMouseEvent) {
-    myPlaces.push({
-        position: event
-    })
-    // 마커가 추가되면 랜더링하면서 localStorage와 동기화한다
-    localStorage.setItem('myPlaces', JSON.stringify(myPlaces))
-}
-
-function renderMarkers () {
-    googleMap.markerList.forEach(m => m.setMap(null)) // 모든 마커 제거
-    googleMap.markerList = []
-    // myPlaces 배열의 요소를 기반으로 마커를 추가한다
-    myPlaces.forEach(place => {
-        const marker = new google.maps.Marker({
-            position: place.position,
-            map: googleMap
-        })
-        googleMap.markerList.push(marker)
-    })
-}
+init();
---
+++ src/type.ts
@@ -0,0 +1,7 @@
+
+export interface position{
+    position : {
+        lat:  number,
+        lng: number,
+    };
+}
